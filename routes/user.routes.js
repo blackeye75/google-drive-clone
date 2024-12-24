965dee335cb954ab08360316f791861e8e794b9d
blackeye75 <priyanshuraj22275@gmail.com>
2024-12-24 22:33:18 +0530

    Implement user login functionality and hash passwords during registration

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator')
 const userModel = require('../models/user.models');
-const bcrypt=require('bcrypt')
+const bcrypt = require('bcrypt')
 
 
 
@@ -13,15 +13,46 @@ router.get('/register', (req, res) => {
 router.post("/register",
     body('email').trim().isEmail().isLength({ min: 10 }),
     body('password').trim().isLength({ min: 5 }),
-    body('username').trim().isLength({ min: 3 }), async (req, res) => {
+    body('username').trim().isLength({ min: 3 }),
+    async (req, res) => {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array(), message: "Invalid Data" })
         }
+
         const { email, username, password } = req.body;
 
-        const newUser = await userModel.create({ email, username, password });
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await userModel.create({
+            email, username, password: hashPassword
+        });
         res.json(newUser);
+    })
+
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+
+router.post('/login',
+    body('username').trim().isLength({ min: 13 }),
+    body('password').trim().isLength({ min: 5 }), async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array(), message: "Invalid Data" })
+        }
+        const { username, password } = req.body;
+        const user = await userModel.findOne({
+            username,
+        })
+        if (!user) {
+            return res.status(400).json({ message: "Invalid Credentials" })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid Credentials" })
+        }
     })
 
 module.exports = router;
