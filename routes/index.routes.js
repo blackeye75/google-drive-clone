82a33f2cd1cd9c2e755c186d6b4b58f07967e0af
blackeye75 <priyanshuraj22275@gmail.com>
2024-12-28 11:55:42 +0530

    done for now

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,19 +1,18 @@
-const express= require('express');
-const {upload, uploadToAppwrite} = require('../config/multer.config')
+const express = require('express');
+const { upload, uploadToAppwrite } = require('../config/multer.config')
 
-const router= express.Router();
+const router = express.Router();
 
-router.get("/home",(req,res)=>{
+router.get("/home", (req, res) => {
     res.render('home');
 })
 
-router.post('/upload',upload.single('file'),uploadToAppwrite,(req,res)=>{
-    res.send(req.file);
-    // res.json({ 
-       
-    //     message: 'File uploaded successfully', 
-    //     fileId: req.fileId
-    // });
+router.post('/upload', upload.single('file'), uploadToAppwrite, (req, res) => {
+    res.json({
+
+        message: 'File uploaded successfully',
+        fileId: req.fileId
+    });
 })
 
 module.exports = router;
